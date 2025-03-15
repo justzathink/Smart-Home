8da9e3952162bb27778435cfac78d3c91c814610
nguyentienphat2904 <phat.nguyen221252729@hcmut.edu.vn>
2025-03-15 17:21:59 +0700

    monitor (API): get data from sensor

--- app/(root)/details/monitor.tsx
+++ app/(root)/details/monitor.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "expo-router";
 
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack } from "expo-router";
-import { Card } from "react-native-paper";
 
 import icons from '@/constants/icons'
+
+import { fetchFeeds, fetchFeedData, fetchMultipleFeeds, SensorDataType } from "@/api/monitor";
+
+const FEEDS = ["TEMPERATURE", "LIGHT"]
 
 type SensorCardProps = {
     icon: any;
@@ -26,6 +29,28 @@ const SensorCard: React.FC<SensorCardProps> = ({ icon, value, label }) => (
 
 const Monitor = () => {
 
+    const [feedValues, setFeedValues] = useState<{ [key: string]: string }>({});
+    const [sensorData, setSensorData] = useState<SensorDataType>({
+        temperature: "N/A",
+        light: "N/A"
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await fetchMultipleFeeds();
+            setSensorData(data);
+        };
+
+        // Gọi API ngay lần đầu
+        fetchData();
+
+        // Thiết lập interval mỗi 3 giây
+        const interval = setInterval(fetchData, 3000);
+
+        // Cleanup interval khi component unmount
+        return () => clearInterval(interval);
+    }, []);
+
     const router = useRouter();
     const handleSensorDetail = (screen: "/details/sensor") => {
         router.push(screen);
@@ -44,11 +69,11 @@ const Monitor = () => {
 
             <View className="flex flex-row">
                 <SensorCard icon={icons.humid} value="6%" label="Độ ẩm" />
-                <SensorCard icon={icons.light} value="332 lux" label="Cường độ ánh sáng" />
+                <SensorCard icon={icons.light} value={`${sensorData.light} lux`} label="Cường độ ánh sáng" />
             </View>
 
             <View className="flex flex-row">
-                <SensorCard icon={icons.tempin} value="25°C" label="Nhiệt độ bên trong" />
+                <SensorCard icon={icons.tempin} value={`${sensorData.temperature}°C`} label="Nhiệt độ bên trong" />
                 <SensorCard icon={icons.tempout} value="32°C" label="Nhiệt độ bên ngoài" />
             </View>
             <TouchableOpacity onPress={() => handleSensorDetail("/details/sensor")} className="items-center p-4 w-[50vw] mx-auto my-5 bg-blue-400 rounded-2xl">
