f3a99a7393a87093fd16993562d57e5d0e1e38d7
duykhangtong <khang.tong1124@hcmut.edu.vn>
2025-04-16 19:46:18 +0700

    test api

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,55 +1,88 @@
-import { View, Text, SafeAreaView, TouchableOpacity, Image} from "react-native";
+import {
+	View,
+	Text,
+	SafeAreaView,
+	TouchableOpacity,
+	Image,
+} from "react-native";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import icons from '@/constants/icons'
+import React, { useEffect, useState } from "react";
+import icons from "@/constants/icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import { useRouter } from "expo-router";
 
 const Index = () => {
-  const router = useRouter();
+	const router = useRouter();
 
-  const handleDetail = (screen: "/details/monitor" | "/details/statistics" | "/details/control" | "/details/door") => {
-    router.push(screen);
-  };
-  
-  
-  return (
-      <SafeAreaView className="flex-1 bg-white px-6 pt-40">
-          <StatusBar style="dark" />
-          <Text className="text-2xl font-bold">Xin chào, User</Text>
-          <Text className="text-gray-500 text-sm">DĨ AN, BÌNH DƯƠNG</Text>
-          <Text className="text-gray-400 text-xs mt-1">26/02/2025</Text>
-          <View className="border-b border-purple my-4" />
-          <Text className="text-lg font-semibold mb-2">Chức năng</Text>
-          <TouchableOpacity onPress={() => handleDetail("/details/monitor")} className="flex-row items-center bg-gray p-4 rounded-2xl my-2">
-            <View className="bg-white rounded-full p-2">
-              <Image source={icons.sun} resizeMode="contain" className="size-6"/>
-            </View>
-            <Text className="ml-4 text-lg font-medium">Giám sát môi trường</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => handleDetail("/details/statistics")} className="flex-row items-center bg-gray p-4 rounded-2xl my-2">
-            <View className="bg-white rounded-full p-2">
-              <Image source={icons.data} resizeMode="contain" className="size-6"/>
-            </View>
-            <Text className="ml-4 text-lg font-medium">Thống kê dữ liệu</Text>
-          </TouchableOpacity>
+	const handleDetail = (
+		screen:
+			| "/details/monitor"
+			| "/details/statistics"
+			| "/details/control"
+			| "/details/door"
+	) => {
+		router.push(screen);
+	};
+	
+	const [username, setUsername] = useState<string | null>("");
 
-          <TouchableOpacity onPress={() => handleDetail("/details/control")} className="flex-row items-center bg-gray p-4 rounded-2xl my-2">
-            <View className="bg-white rounded-full p-2">
-              <Image source={icons.sun} resizeMode="contain" className="size-6"/>
-            </View>
-            <Text className="ml-4 text-lg font-medium">Điều Khiển</Text>
-          </TouchableOpacity>
+	useEffect(() => {
+		const fetchUsername = async () => {
+			const storedUsername = await AsyncStorage.getItem("username");
+			setUsername(storedUsername);
+		};
+		fetchUsername();
+	}, []);
 
-          <TouchableOpacity onPress={() => handleDetail("/details/door")} className="flex-row items-center bg-gray p-4 rounded-2xl my-2">
-            <View className="bg-white rounded-full p-2">
-              <Image source={icons.key} resizeMode="contain" className="size-6"/>
-            </View>
-            <Text className="ml-4 text-lg font-medium">Quản lý cửa</Text>
-          </TouchableOpacity>
+	return (
+		<SafeAreaView className='flex-1 bg-white px-6 pt-40'>
+			<StatusBar style='dark' />
+			<Text className='text-2xl font-bold'>Xin chào, {username}</Text>
+			<Text className='text-gray-500 text-sm'>DĨ AN, BÌNH DƯƠNG</Text>
+			<Text className='text-gray-400 text-xs mt-1'>26/02/2025</Text>
+			<View className='border-b border-purple my-4' />
+			<Text className='text-lg font-semibold mb-2'>Chức năng</Text>
+			<TouchableOpacity
+				onPress={() => handleDetail("/details/monitor")}
+				className='flex-row items-center bg-gray p-4 rounded-2xl my-2'
+			>
+				<View className='bg-white rounded-full p-2'>
+					<Image source={icons.sun} resizeMode='contain' className='size-6' />
+				</View>
+				<Text className='ml-4 text-lg font-medium'>Giám sát môi trường</Text>
+			</TouchableOpacity>
+			<TouchableOpacity
+				onPress={() => handleDetail("/details/statistics")}
+				className='flex-row items-center bg-gray p-4 rounded-2xl my-2'
+			>
+				<View className='bg-white rounded-full p-2'>
+					<Image source={icons.data} resizeMode='contain' className='size-6' />
+				</View>
+				<Text className='ml-4 text-lg font-medium'>Thống kê dữ liệu</Text>
+			</TouchableOpacity>
 
-      </SafeAreaView>
-  );
+			<TouchableOpacity
+				onPress={() => handleDetail("/details/control")}
+				className='flex-row items-center bg-gray p-4 rounded-2xl my-2'
+			>
+				<View className='bg-white rounded-full p-2'>
+					<Image source={icons.sun} resizeMode='contain' className='size-6' />
+				</View>
+				<Text className='ml-4 text-lg font-medium'>Điều Khiển</Text>
+			</TouchableOpacity>
+
+			<TouchableOpacity
+				onPress={() => handleDetail("/details/door")}
+				className='flex-row items-center bg-gray p-4 rounded-2xl my-2'
+			>
+				<View className='bg-white rounded-full p-2'>
+					<Image source={icons.key} resizeMode='contain' className='size-6' />
+				</View>
+				<Text className='ml-4 text-lg font-medium'>Quản lý cửa</Text>
+			</TouchableOpacity>
+		</SafeAreaView>
+	);
 };
 
 export default Index;
-
---
+++ constants/config.ts
@@ -0,0 +1,3 @@
+export default {
+	BASE_URL: "http://192.168.1.26:3000/api",
+};
