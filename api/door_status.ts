0f40996efb72c04a83cc7e223f9c87d2978eac53
duykhangtong <khang.tong1124@hcmut.edu.vn>
2025-04-22 15:02:43 +0700

    API: modify door and password api

--- api/door_status.ts
+++ api/door_status.ts
@@ -1,28 +1,36 @@
 // api/updateDoorStatus.ts
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import config from "@/constants/config";
 
-const ADAFRUIT_USERNAME = 'khangnguyen2k4';
-const ADAFRUIT_KEY = 'aio_fnev41LugFWmzdJcQvX4kjJuexeQ';
-const FEED_NAME = 'assignment.pass';
+// const ADAFRUIT_USERNAME = 'khangnguyen2k4';
+// const ADAFRUIT_KEY = 'aio_fnev41LugFWmzdJcQvX4kjJuexeQ';
+// const FEED_NAME = 'assignment.pass';
 
 export const updateDoorStatus = async (status: number) => {
-  try {
-    const response = await axios.post(
-      `https://io.adafruit.com/api/v2/${ADAFRUIT_USERNAME}/feeds/${FEED_NAME}/data`,
-      {
-        value: status,
-      },
-      {
-        headers: {
-          'X-AIO-Key': ADAFRUIT_KEY,
-          'Content-Type': 'application/json',
-        },
-      }
-    );
+	try {
+		// const response = await axios.post(
+		//   `https://io.adafruit.com/api/v2/${ADAFRUIT_USERNAME}/feeds/${FEED_NAME}/data`,
+		//   {
+		//     value: status,
+		//   },
+		//   {
+		//     headers: {
+		//       'X-AIO-Key': ADAFRUIT_KEY,
+		//       'Content-Type': 'application/json',
+		//     },
+		//   }
+		// );
 
-    return response.data;
-  } catch (error) {
-    console.error("Lỗi khi cập nhật trạng thái cửa lên Adafruit:", error);
-    return null;
-  }
+		// return response.data;
+		const token = await AsyncStorage.getItem("token");
+		await axios.post(
+			`${config.BASE_URL}/control/door`,
+			{ status },
+			{ headers: { Authorization: `Bearer ${token}` } }
+		);
+	} catch (error) {
+		console.error("Lỗi khi cập nhật trạng thái cửa lên Adafruit:", error);
+		return null;
+	}
 };
--- api/password.ts
+++ api/password.ts
@@ -1,36 +1,56 @@
-const AIO_USERNAME = "khangnguyen2k4";
-const FEED_KEY = "assignment.password";
-const AIO_KEY = "aio_fnev41LugFWmzdJcQvX4kjJuexeQ";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import config from "@/constants/config";
+
+// const AIO_USERNAME = "khangnguyen2k4";
+// const FEED_KEY = "assignment.password";
+// const AIO_KEY = "aio_fnev41LugFWmzdJcQvX4kjJuexeQ";
 
 export const getCorrectPin = async (): Promise<string | null> => {
-    try {
-        const res = await fetch(`https://io.adafruit.com/api/v2/${AIO_USERNAME}/feeds/${FEED_KEY}/data/last`, {
-            headers: {
-                "X-AIO-Key": AIO_KEY
-            }
-        });
-        const data = await res.json();
-        return data.value;
-    } catch (error) {
-        console.error("Failed to fetch correct pin:", error);
-        return null;
-    }
+	try {
+		// const res = await fetch(`https://io.adafruit.com/api/v2/${AIO_USERNAME}/feeds/${FEED_KEY}/data/last`, {
+		//     headers: {
+		//         "X-AIO-Key": AIO_KEY
+		//     }
+		// });
+		// const data = await res.json();
+		// return data.value;
+		const token = await AsyncStorage.getItem("token");
+		const res = await axios.get(`${config.BASE_URL}/control/pin`, {
+			headers: { Authorization: `Bearer ${token}` },
+		});
+
+		return res.data.value;
+	} catch (error) {
+		console.error("Failed to fetch correct pin:", error);
+		return null;
+	}
 };
 
 export const updateCorrectPin = async (newPin: string): Promise<boolean> => {
-    try {
-        const res = await fetch(`https://io.adafruit.com/api/v2/${AIO_USERNAME}/feeds/${FEED_KEY}/data`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "X-AIO-Key": AIO_KEY
-            },
-            body: JSON.stringify({ value: newPin })
-        });
+	try {
+		// const res = await fetch(
+		// 	`https://io.adafruit.com/api/v2/${AIO_USERNAME}/feeds/${FEED_KEY}/data`,
+		// 	{
+		// 		method: "POST",
+		// 		headers: {
+		// 			"Content-Type": "application/json",
+		// 			"X-AIO-Key": AIO_KEY,
+		// 		},
+		// 		body: JSON.stringify({ value: newPin }),
+		// 	}
+		// );
 
-        return res.ok;
-    } catch (error) {
-        console.error("Failed to update pin:", error);
-        return false;
-    }
+		// return res.ok;
+		const token = await AsyncStorage.getItem("token");
+		const res = await axios.post(
+			`${config.BASE_URL}/control/pin`,
+			{ newPin },
+			{ headers: { Authorization: `Bearer ${token}` } }
+		);
+        return res.data.success;
+	} catch (error) {
+		console.error("Failed to update pin:", error);
+		return false;
+	}
 };
